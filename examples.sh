3c1a0b8f35611d1c73585ba827ade33199778003
Marco De La Pierre <marco.delapierre@gmail.com>
2022-07-06 09:59:41 +0800

    organised input/test-output files

--- examples.sh
+++ examples.sh
@@ -4,4 +4,4 @@ AWS_PROFILE='pawsey0001' nextflow run main.nf
 
 nextflow run main.nf --input='s3://pawsey0001-marco-personal/input_user_s3'
 
-nextflow run main.nf --input='./input_local'
+nextflow run main.nf --input='inputs/input_local'
