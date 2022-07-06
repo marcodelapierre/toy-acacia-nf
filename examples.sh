1a18204c35d608edb4494171ef26a6ec15492921
Marco De La Pierre <marco.delapierre@gmail.com>
2022-07-06 10:42:00 +0800

    updated examples

--- examples.sh
+++ examples.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-AWS_PROFILE='pawsey0001' nextflow run main.nf
+# Suitable for installations on personal computers (laptop, workstation, cloud)
 
-nextflow run main.nf --input='s3://pawsey0001-marco-personal/input_user_s3'
+# Case 1. use default NF params.input and default S3 profile
+nextflow run main.nf
 
-nextflow run main.nf --input='inputs/input_local'
+# Case 2. use non-default NF params.input and non-default S3 profile
+AWS_PROFILE='pawsey0001' nextflow run main.nf \
+  --input='s3://pawsey0001-marco-store/input_project_s3'
+
+# Case 3. use local (non S3) params.input
+nextflow run main.nf \
+  --input='inputs/input_local'
